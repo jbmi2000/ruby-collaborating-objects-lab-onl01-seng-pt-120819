c1dd22fca2e953632cd72494c26026c74c736854
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-01-23 02:21:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,21 +1,21 @@
 # require 'pry'
 # class Artist
-# 
+#
 #   attr_accessor :name
-# 
+#
 #   @@all = []
-# 
+#
 #   def initialize(name)
 #     @name = name
 #   #  @@all << artist.self
-# 
+#
 #   end
-# 
+#
 #   def self.all
 #     @@all
 #   end
 # end
-# 
+#
 # # binding.pry
 class Artist
 
